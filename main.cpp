43c2c0e3b5cad99cfc69cb3937477810ff29ac55
helene <helene.maja@hotmail.dk>
2025-02-14 20:10:36 +0100

    add adc measurement

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 
 int main()
 {
     stdio_init_all();
+    adc_init();
+    adc_gpio_init(26);
+    adc_select_input(0);
+    constexpr float conversion_factor = 3.3f / (1 << 12);
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    while (true) 
+    {
+        uint16_t result = adc_read();
+        printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
+        sleep_ms(500);
     }
 }
